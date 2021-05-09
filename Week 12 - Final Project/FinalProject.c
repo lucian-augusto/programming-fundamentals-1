113cd5046bb08f5e998bcbd5de904d1b33675626
lucian-augusto <lucian.augusto.92@gmail.com>
2021-05-09 20:28:38 -0300

    Improve Function prototypes header.

--- Week 12 - Final Project/FinalProject.c
+++ Week 12 - Final Project/FinalProject.c
@@ -41,17 +41,15 @@ typedef struct {
 /* =============================================================================
  * Function Prototypes Header
  * ========================================================================== */
-// Syscall Functions/Utilities (Compatible with Linux, MacOS and Windows)
-void callSleep(int timeInMs);
-void clearScreen();
-// Memory Utilities
-char **alocaMatriz(int nL, int nC);
-int checkMemoryAllocation(char **a, int numberLines);
-void desalocaMatriz(char **m, int nL);
-// Matrix Utilities
-void copiaMatriz(char **copy, char **original, int nL, int nC);
-void imprimeMatriz(char **m, int nL, int nC);
-int isInRange(int number, int dim);
+// Game Initialization Functions
+void callSplashScreen();
+int captureBoardParam(char *paramName, int min, int max);
+void captureGameOptions(Game *g);
+void menuInicJogo(char **mat, int nL, int nC);
+void newGame(Game *g, int isReplay);
+void playGame(Game *g);
+int replayGame(int *keepOptions);
+int shouldKeepOptions();
 // Board Initialization Functions
 void customInit(char **board, int numberLines, int numberColumns);
 void inicBlinker(char **m, int nL, int nC);
@@ -68,15 +66,17 @@ void createNextGenerationBoard(char **nextGenBoard, char **currentGenBoard,
 char evaluateCell(char **board, int currentLine, int currentColumn,
                   int numberLines, int numberColumns);
 void jogaJogoVida(char **m, int nL, int nC, int generationAmount);
-// Game Initialization Functions
-void callSplashScreen();
-int captureBoardParam(char *paramName, int min, int max);
-void captureGameOptions(Game *g);
-void menuInicJogo(char **mat, int nL, int nC);
-void newGame(Game *g, int isReplay);
-void playGame(Game *g);
-int replayGame(int *keepOptions);
-int shouldKeepOptions();
+// Syscall Functions/Utilities (Compatible with Linux, MacOS and Windows)
+void callSleep(int timeInMs);
+void clearScreen();
+// Memory Utilities
+char **alocaMatriz(int nL, int nC);
+int checkMemoryAllocation(char **a, int numberLines);
+void desalocaMatriz(char **m, int nL);
+// Matrix Utilities
+void copiaMatriz(char **copy, char **original, int nL, int nC);
+void imprimeMatriz(char **m, int nL, int nC);
+int isInRange(int number, int dim);
 
 /* =============================================================================
  * Main 
